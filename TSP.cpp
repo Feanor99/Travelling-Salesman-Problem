25b12b8a5fcc3ef74bbe8f42963273a9af4fa448
Yunus Dilber <yunusdilber99@gmail.com>
2020-01-05 23:48:00 +0300

    this is my message from the past

--- TSP.cpp
+++ TSP.cpp
@@ -9,6 +9,9 @@
 // SEYYAR SATICI PROBLEMI - TRAVELLING SALESMAN PROBLEM
 
 //a(0,0)b(1,0)c(2,0)d(3,0)e(0,1)f(1,1)g(2,1)h(3,1)i(0,2)j(1,2)
+
+//this file will be master
+
 using namespace std;
 
 int factorial(int n)
